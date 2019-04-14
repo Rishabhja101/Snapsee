66b19ca862cf37a1c2efc07b6a7a6c0e56848768
Kyle Mumma <kyle.mumma@live.com>
2019-04-13 22:34:12 -0700

    dark theme

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,7 +4,7 @@ const MainPage = (props) => {
     console.log(props.data);
     return (
         <div className="text-center">
-            <h2 className="display-4">Welcome to Snapsee {props.data.data.me.displayName}</h2>
+            <h2 className="display-4">Welcome to Snapsee <span style={{color: 'yellow'}}>{props.data.data.me.displayName}</span></h2>
             <img className="mb-4" src={props.data.data.me.bitmoji.avatar} alt="bitmoji of user"/>
 
             <form action="/" method="post">
@@ -12,6 +12,7 @@ const MainPage = (props) => {
                     Upload an image of desired friends' face
                 </label><br/>
                 <input type="file" accept="image/*" id="image-upload"/>
+                <button type="submit" value="submit">Upload Image</button>
             </form>
         </div>
     );
