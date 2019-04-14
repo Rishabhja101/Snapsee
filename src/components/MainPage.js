5fb76c7a8cdb7de2b259edddb8a954e47e34fc1f
Kyle Mumma <kyle.mumma@live.com>
2019-04-13 22:01:46 -0700

    upload button added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -5,7 +5,14 @@ const MainPage = (props) => {
     return (
         <div className="text-center">
             <h2 className="display-4">Welcome to Snapsee {props.data.data.me.displayName}</h2>
-            <img src={props.data.data.me.bitmoji.avatar} alt="bitmoji of user"/>
+            <img className="mb-4" src={props.data.data.me.bitmoji.avatar} alt="bitmoji of user"/>
+
+            <form action="/" method="post">
+                <label for="image-upload" className="lead">
+                    Upload an image of desired friends' face
+                </label><br/>
+                <input type="file" accept="image/*" id="image-upload"/>
+            </form>
         </div>
     );
 };
